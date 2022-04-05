223aee993b8580f35fffa539121afece2466bf98
Ando Rafanomezantsoa <rafandonia@outlook.fr>
2022-04-05 19:27:42 +0200

    premier commit!

---
+++ u_at_ord_2.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO BIB_OUVRAGES (titre,isbn) VALUES('Unique constraint', 23);
+
+
+INSERT INTO BIB_Exemplaires (isbn,numero,etat) VALUES(23,2,DEFAULT);
+SELECT * FROM BIB_Exemplaires;
+
+INSERT INTO BIB_GENRES  VALUES('NEWS','ouvrage d''aujourd''hui');
+
